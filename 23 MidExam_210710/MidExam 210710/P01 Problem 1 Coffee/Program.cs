802bba1795b503df682a705a7398170c89fdbf16
Irina Georgieva <georgieva2490@gmail.com>
2021-12-19 13:01:10 +0100

    Added mid exam Fundamentals

---
+++ 23 MidExam_210710/MidExam 210710/P01 Problem 1 Coffee/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P01_Problem_1_Coffee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ordersNumber = int.Parse(Console.ReadLine());
+
+            double totalPrice = 0;
+
+            for (int i = 0; i < ordersNumber; i++)
+            {
+                double priceCapsule = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsulesCount = int.Parse(Console.ReadLine());
+
+                double priceCoffee = (days * capsulesCount) * priceCapsule;
+
+                Console.WriteLine($"The price for the coffee is: ${priceCoffee:F2}");
+
+                totalPrice += priceCoffee;
+            }
+
+            Console.WriteLine($"Total: ${totalPrice:F2}");
+        }
+    }
+}
